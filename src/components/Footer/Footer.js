b50af21f1f98f3c83ad4ec761874ef85c40c6ae7
Ridoyb <ridoyb8730@gmail.com>
2022-10-25 21:07:24 +0600

    Footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-dark pt-2 pb-2 '>
+        <div className='bg-dark pt-2 pb-2 text-center'>
             <p>Copyright: North Tech All Right Reserved.</p>
         </div>
     );
